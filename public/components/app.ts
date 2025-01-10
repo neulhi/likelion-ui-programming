89e944f0a910658fa9e0c4ac5002e90dfa1732e7
neulhi <neu1hi.work@gmail.com>
2025-01-10 13:47:13 +0900

    [실습] Calcurator 컴포넌트 part 2. - 컴포넌트 속성(props)에 따른 출력 변경 구현

--- public/components/app.ts
+++ public/components/app.ts
@@ -1,11 +1,18 @@
 import React from '../lib/react.js';
 import Calculator from './calculator.js';
 
-
 function App() {
   return React.createElement('div', {
     className: 'app',
-		children: React.createElement(Calculator),
+    children: [
+      React.createElement(Calculator),
+      React.createElement(Calculator, {
+        operator: '-',
+      }),
+			React.createElement(Calculator, {
+				elements: [10, 2],
+      }),
+    ],
   });
 }
 
