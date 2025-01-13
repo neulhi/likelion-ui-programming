aa3e34a2678afe10dbf938daad477ca20cad5e1d
neulhi <neu1hi.work@gmail.com>
2025-01-13 13:51:30 +0900

    [실습] Switch 컴포넌트 만들기 04-2 - 비활성 상태 스타일링

--- frontend/components/switch.ts
+++ frontend/components/switch.ts
@@ -3,7 +3,7 @@
 // --------------------------------------------------------------------------
 // - [x] switch 역할(role) 설정
 // - [x] aria-checked 속성을 사용해 ON/OFF 상태 설정
-// - [ ] ON/OFF 텍스트를 화면에 표시하더라도 aria-hidden 속성을 사용해 읽지 않도록 설정
+// - [x] ON/OFF 텍스트를 화면에 표시하더라도 aria-hidden 속성을 사용해 읽지 않도록 설정
 // - [ ] 사용자가 Space, Enter(옵션) 키를 눌렀을 때 작동되도록 설정
 // - [ ] 비활성 상태인 경우, aria-disabled 속성을 사용해 설정
 // --------------------------------------------------------------------------
@@ -38,18 +38,19 @@ function Switch({
     ? h('span', { className: 'Switch--text', 'aria-hidden': true }, switchText)
     : null;
 
-  return h('div', {
-    role: 'switch',
-    'aria-checked': active,
-    'aria-disabled': disabled,
-    tabIndex: 0,
-    className: 'Switch',
-    children: [
-      h('span', { className: 'Switch--label' }, children),
-      h('span', { className: 'Switch--knob' }),
-      switchTextNode,
-    ],
-  });
+  return h(
+    'div',
+    {
+      role: 'switch',
+      'aria-checked': active,
+      'aria-disabled': disabled,
+      tabIndex: 0,
+      className: 'Switch',
+    },
+    h('span', { className: 'Switch--label' }, children),
+    h('span', { className: 'Switch--knob' }),
+    switchTextNode
+  );
 }
 
 export default Switch;
