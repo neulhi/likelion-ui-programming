3334f81b17da6a5cfa94f51cfb6bbe54f5794fcf
neulhi <neu1hi.work@gmail.com>
2025-01-16 14:36:59 +0900

    [실습] React 클래스 컴포넌트 vs. 함수 컴포넌트

--- frontend/scripts/app.tsx
+++ frontend/scripts/app.tsx
@@ -1,26 +1,27 @@
 import React from '../lib/react.js';
 import SwitchList from '../components/switch-list.tsx';
+import SwitchListClass from '../components/switch-list.class.tsx';
 
 function App(): React.ReactNode {
   const list = [
     {
-			id: 'item-1',
+      id: 'item-1',
       active: true,
       onToggle: () => console.log('clicked switch 1'),
       children: '와이파이(Wi-Fi)',
     },
     {
-			id: 'item-2',
+      id: 'item-2',
       children: '블루투스(Bluetooth)',
     },
     {
-			id: 'item-3',
+      id: 'item-3',
       disabled: true,
       onToggle: () => console.log('clicked switch 3'),
       children: '동작 줄이기(Reduced Motion)',
     },
     {
-			id: 'item-4',
+      id: 'item-4',
       children: '스크린 리더(Screen Reader)',
       active: true,
     },
@@ -28,6 +29,17 @@ function App(): React.ReactNode {
 
   return (
     <div lang="en" style={appStyles}>
+      {/* 클래스 컴포넌트 */}
+      <SwitchListClass items={list} />
+      <hr
+        style={{
+          display: 'block',
+          width: '100%',
+          height: 1,
+          borderTop: '1px solid rgba(0 0 0 / 40%)',
+        }}
+      />
+      {/* 함수형 컴포넌트 */}
       <SwitchList items={list} />
     </div>
   );
